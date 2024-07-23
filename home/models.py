5289e7287326910c3f92450f13287c683eb6c87c
Abhi <abhijoshi1441@gmail.com>
2024-07-23 15:46:08 +1000

    Designed Contact Us form and its backend
    1. Created Contact Us form.
    2. Created Corresponding database to store information from contact us form.
    3. Changed CSS template to bootstrap@5.3.3 from 4.5.2

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField(default='No description provided')
+    date = models.DateField(default=datetime.now)
+    
+    def __str__(self):
+        return self.name
